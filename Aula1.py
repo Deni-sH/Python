55c7a24f73fb0bef8c583ade2b390cfff957f9a9
Denis <denis_hara@hotmail.com>
2023-02-09 15:07:07 -0300

    new

---
+++ Aula1.py
@@ -0,0 +1,42 @@
+
+# similaridades com c 
+# soma=5.5
+# print("soma=",soma)
+
+o que é ciência da computação
+
+nova ciencia, engloba várias áreas do conhecimento humano
+matemática :diferentes aspectos computacionais
+
+engenharia: contruir sistemas computacionais complexos, software, hardware
+ciencias naturais: desenvolveram uma metodologia cientificas, hipoteses, teorias prever novos comportamentos/ experimentos
+arte: aspectos criativos, estética novas ideais codificadas na forma de software
+esporte: praticar bastante, manter resiliência para se manter fluente na linguagem.
+
+habiliaddes: capacidade de resolver problemas do mundo real computacionalmente.
+Formular um problema do mundo real em termos computacionais
+
+Elaborar uma solução para esse problema em termos computacionais.
+*algoritmo
+
+Escrever um programa em uma linguagem de programação que implemente esse algoritmo
+Testar o programa para verificar se ele realmente resolve esse problema
+
+habilidades mais avançadas.
+
+>Software de grande porte composto por muitos programas
+>Grandes quantidades de dados
+>Gerenciar equipes de desenvolvimento de software
+
+Comunicar-se com clientes e usuarios para entender seus problemas, dificuldades e necessidades
+
+
+Pergunta 5
+compilador é responsável por traduzir programas escritos em linguagem de alto nível para linguagem de máquina.
+
+Pergunta 2
+Software e programas(hardware não é) são instruções para o computador. (escolha todas as opções corretas)
+
+
+
+
